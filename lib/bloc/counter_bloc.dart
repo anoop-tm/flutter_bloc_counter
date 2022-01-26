18f91cd4e0b99ba81c1eb1d81b6d686e64184a9c
Anoop <hi@anoop.work>
2022-01-26 21:23:58 +0530

    Add bloc files

---
+++ lib/bloc/counter_bloc.dart
@@ -0,0 +1,16 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'counter_event.dart';
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  CounterBloc() : super(0) {
+    on<CounterIncremented>((event, emit) {
+      emit(state + 1);
+    });
+
+    on<CounterDecremented>((event, emit) {
+      emit(state - 1);
+    });
+  }
+}
